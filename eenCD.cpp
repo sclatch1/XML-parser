fb0d43f1e2b99b79cb001c130d6730c259ea67b7
v <zeppi_IP@protonmail.com>
2022-03-04 09:52:51 +0100

    main voor vraag 1

--- eenCD.cpp
+++ eenCD.cpp
@@ -6,55 +6,29 @@
 
 string eenCD::get_Artist() {return Artist;}
 
-void eenCD::set_Artist(string &artist)
-{
-
-    Artist = artist;
-}
+void eenCD::set_Artist(string &artist) {Artist = artist;}
 
 string eenCD::get_Title(){return Title;}
 
-void eenCD::set_Title(string &title)
-{
-    Title = title;
-}
+void eenCD::set_Title(string &title) {Title = title;}
 
 int eenCD::get_Year() {return Year;}
 
-void eenCD::set_Year(int year)
-{
-    Year = year;
-}
+void eenCD::set_Year(int year) {Year = year;}
 
 int eenCD::get_Price() {return Price;}
 
-void eenCD::set_Price(int price)
-{
-    Price = price;
-}
+void eenCD::set_Price(int price) {Price = price;}
 
-string eenCD::get_Company()
-{
-    return Company;
-}
+string eenCD::get_Company() {return Company;}
 
-void eenCD::set_Company(string company)
-{
-    Company = company;
-}
+void eenCD::set_Company(string company) {Company = company;}
 
-string eenCD::get_Country()
-{
-    return Country;
-}
+string eenCD::get_Country(){return Country;}
 
-void eenCD::set_Country(string coutry)
-{
-    Country = coutry;
-}
+void eenCD::set_Country(string coutry) {Country = coutry;}
 
-string eenCD::to_string()
-{
+string eenCD::to_string(){
     string output = "it worked";
     cout << Title << endl << Artist << endl << Company << endl << Country << endl <<Price << Year << endl;
     return output;
--- main.cpp
+++ main.cpp
@@ -8,45 +8,29 @@
 using namespace std;
 
 
-
-int main()
-{
+//Voor oef 1:
+int main() {
     eenCD cd;
     TiXmlDocument doc;
-    if(!doc.LoadFile("cd.xml"))
-    {
+    if (!doc.LoadFile("eenCD.xml")) {
         std::cerr << doc.ErrorDesc() << std::endl;
-
+        return 1;
     }
-    TiXmlElement* root = doc.FirstChildElement();
-    if (root == NULL)
-    {
-        std::cerr << "Failed to load file: No root element" << std::endl;
+    //date uit doc halen
+    TiXmlElement *CD = doc.FirstChildElement();
+
+    if (CD == NULL) {
+        std::cerr << "Failed to load file: No root element." << std::endl;
         doc.Clear();
-
+        return 1;
     }
-    for(TiXmlElement* elem = root->FirstChildElement(); elem != NULL; elem = elem->NextSiblingElement())
-    {
-        string elemName = elem->Value();
-        string el_text = elem->GetText();
-        if(elemName == "TITLE")
-        {
-            cd.set_Title(el_text);
+    for (TiXmlElement *elem = CD->FirstChildElement(); elem != NULL; elem = elem->NextSiblingElement()) {
+        string s = elem->Value();
+        if (s == "TITLE") {
+            cout << elem->Value() << ": " << elem->GetText() << endl;
         }
-        else if(elemName == "ARTIST")
-        {
-            cd.set_Artist(el_text);
+        if (s == "ARTIST") {
+            cout << elem->Value() << ": " << elem->GetText() << endl;
         }
-        else if(elemName == "COUNTRY")
-        {
-            cd.set_Country(el_text);
-        }
-        else if(elemName == "COMPANY")
-        {
-            cd.set_Company(el_text);
-        }
-
     }
-    cd.to_string();
 }
-
